7f0dfabc8f09c9edcfb71b4dd0ef97dc8cd216ea
Travis Holland <travisjholland@gmail.com>
2022-02-04 22:08:47 -0800

    update before bed

--- contracts/Grid.sol
+++ contracts/Grid.sol
@@ -2,28 +2,37 @@
 pragma solidity ^0.7.0;
 
 import "hardhat/console.sol";
-import "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
-import "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
-import "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+import {
+    ISuperfluid,
+    ISuperToken,
+    ISuperApp,
+    ISuperAgreement,
+    SuperAppDefinitions
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+
+import {
+    SuperAppBase
+} from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperAppBase.sol";
+
+import {
+    IConstantFlowAgreementV1
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
 
 contract Grid is SuperAppBase {
-    ISuperFluid private _host; // host
+    ISuperfluid private _host; // host
     IConstantFlowAgreementV1 private _cfa; // the stored constant flow agreement
     ISuperToken private _acceptedToken; // accepted token
     address private _receiver;
 
     constructor(
-        ISuperFluid host,
-        ICOnstantFlowAgreementV1 cfa,
+        ISuperfluid host,
+        IConstantFlowAgreementV1 cfa,
         ISuperToken acceptedToken,
         address receiver
     ) {
         require(address(host) != address(0), "host is zero address");
         require(address(cfa) != address(0), "cfa is zero address");
-        require(
-            address(acceptedToken) != address(0),
-            "acceptedToken is zero address"
-        );
+        require(address(acceptedToken) != address(0), "acceptedToken is zero address");
         require(address(receiver) != address(0), "receiver is zero address");
         require(!host.isApp(ISuperApp(receiver)), "receiver is an app");
 
@@ -44,19 +53,129 @@ contract Grid is SuperAppBase {
      * SuperApp callbacks
      *************************************************************************/
     
-    function afterAgreementCreate(
-        ISuperToken _superToken,
-        address _agreementClass,
-        bytes32, // _agreementId,
-        bytes calldata, /*_agreementData*/
-        bytes calldata, // _cbdata,
-        bytes calldata _ctx
+    function beforeAgreementCreated(
+        ISuperToken superToken,
+        address agreementClass,
+        bytes32 /*agreementId*/,
+        bytes calldata /*agreementData*/,
+        bytes calldata ctx
     )
-        external
-        overrideonlyExpected(_superToken, _agreementClass)
+        external view override
+        onlyHost
+        onlyExpected(superToken, agreementClass)
+        returns (bytes memory cbdata)
+    {
+        // TODO: my logic here
+        console.log("beforeAgreementCreated callback");
+    }
+
+    function afterAgreementCreated(
+        ISuperToken /* superToken */,
+        address agreementClass,
+        bytes32 agreementId,
+        bytes calldata /*agreementData*/,
+        bytes calldata cbdata,
+        bytes calldata ctx
+    )
+        external override
         onlyHost
         returns (bytes memory newCtx)
     {
-        return _updateOutflow(_ctx); // I think my logic goes here
+        // TODO: my logic here
+        console.log("afterAgreementCreated callback");
+    }
+
+    function beforeAgreementUpdated(
+        ISuperToken superToken,
+        address agreementClass,
+        bytes32 /*agreementId*/,
+        bytes calldata /*agreementData*/,
+        bytes calldata ctx
+    )
+        external view override
+        onlyHost
+        onlyExpected(superToken, agreementClass)
+        returns (bytes memory cbdata)
+    {
+        // TODO: my logic here
+        console.log("beforeAgreementUpdated callback");
+    }
+
+    function afterAgreementUpdated(
+        ISuperToken /* superToken */,
+        address agreementClass,
+        bytes32 agreementId,
+        bytes calldata /*agreementData*/,
+        bytes calldata cbdata,
+        bytes calldata ctx
+    )
+        external override
+        onlyHost
+        returns (bytes memory newCtx)
+    {
+        // TODO: my logic here
+        console.log("afterAgreementUpdated callback");
+    }
+
+    function beforeAgreementTerminated(
+        ISuperToken superToken,
+        address agreementClass,
+        bytes32 /*agreementId*/,
+        bytes calldata /*agreementData*/,
+        bytes calldata /*ctx*/
+    )
+        external view override
+        onlyHost
+        returns (bytes memory cbdata)
+    {
+        // According to the app basic law, we should never revert in a termination callback
+        if (!_isSameToken(superToken) || !_isCFAv1(agreementClass)) return abi.encode(true);
+        return abi.encode(false);
+        // TODO: my logic here
+        console.log("beforeAgreementTerminated callback");
+    }
+
+    ///
+    function afterAgreementTerminated(
+        ISuperToken /* superToken */,
+        address /* agreementClass */,
+        bytes32 /* agreementId */,
+        bytes calldata agreementData,
+        bytes calldata cbdata,
+        bytes calldata ctx
+    )
+        external override
+        onlyHost
+        returns (bytes memory newCtx)
+    {
+        // According to the app basic law, we should never revert in a termination callback
+        (bool shouldIgnore) = abi.decode(cbdata, (bool));
+        if (shouldIgnore) return ctx;
+        // note that msgSender can be either flow sender, receiver or liquidator
+        // one must decode agreementData to determine who is the actual player
+        // TODO: my logic here
+        console.log("beforeAgreementTerminated callback");
+        /*(address player, ) = abi.decode(agreementData, (address, address));
+        return _quit(player, ctx);*/
+    }
+
+    function _isSameToken(ISuperToken superToken) private view returns (bool) {
+        return address(superToken) == address(_acceptedToken);
+    }
+
+    function _isCFAv1(address agreementClass) private view returns (bool) {
+        return ISuperAgreement(agreementClass).agreementType()
+            == keccak256("org.superfluid-finance.agreements.ConstantFlowAgreement.v1");
+    }
+
+    modifier onlyHost() {
+        require(msg.sender == address(_host), "Grid: support only one host");
+        _;
+    }
+
+    modifier onlyExpected(ISuperToken superToken, address agreementClass) {
+        require(_isSameToken(superToken), "Grid: not accepted token");
+        require(_isCFAv1(agreementClass), "Grid: only CFAv1 supported");
+        _;
     }
 }
